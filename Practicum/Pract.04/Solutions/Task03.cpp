d75a58ab14355fb502406d9a2f0377550cb42c5d
NikolinaMadzharova <nikolina.madjarova@abv.bg>
2025-10-24 18:24:02 +0300

    Added solutions

--- Practicum/Pract.04/Solutions/Task03.cpp
+++ Practicum/Pract.04/Solutions/Task03.cpp
@@ -12,22 +12,29 @@ char toUpper(char symbol) {
 	if (isLower(symbol)) {
 		return symbol - 'a' + 'A';
 	}
+	else if (isUpper(symbol)) {
+		return symbol;
+	}
 
-	return symbol;
+	return '\0';
 }
 
 char toLower(char symbol) {
 	if (isUpper(symbol)) {
 		return symbol - 'A' + 'a';
 	}
+	else if (isLower(symbol)) {
+		return symbol;
+	}
 
-	return symbol;
+	return '\0';
 }
 
 int main()
 {
 	std::cout << toLower('H') << std::endl;
 	std::cout << toUpper('d') << std::endl;
+	std::cout << toUpper('&') << std::endl;
 	std::cout << toUpper('Y') << std::endl;
 }
 
